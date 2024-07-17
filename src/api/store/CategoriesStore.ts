0de8d12043b0be3e3a517c6e8238607a89f23318
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-17 23:31:15 +0300

    - reworked categories store - added more logic and functionality

--- src/api/store/CategoriesStore.ts
+++ src/api/store/CategoriesStore.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand';
 import CategoriesService from '@/api/services/CategoriesService';
-import { CategoryResponseModel } from '@/api/models/CategoriesModels';
+import {
+    CategoryResponseModel,
+    GetOneCategoryResponseModel,
+} from '@/api/models/CategoriesModels';
 import { AxiosResponse } from 'axios';
+import { SetStateAction } from 'react';
 
 interface CategoryStoreTypes {
     categoryId: string | null;
     setCategoryId: (categoryId: string | null) => void;
+    isOpenCategories: boolean;
+    setIsOpenCategories: (isOpenCategories: SetStateAction<boolean>) => void;
     categories: CategoryResponseModel[] | null;
     getCategories: () => Promise<AxiosResponse<CategoryResponseModel[]>>;
+    getOneCategory: (
+        id: string | null,
+    ) => Promise<AxiosResponse<GetOneCategoryResponseModel>>;
 }
 
 const useCategoryStore = create<CategoryStoreTypes>(set => ({
@@ -15,10 +24,28 @@ const useCategoryStore = create<CategoryStoreTypes>(set => ({
     setCategoryId: id => set({ categoryId: id }),
     categories: null,
 
+    isOpenCategories: false,
+    setIsOpenCategories: isOpenCategories =>
+        set(state => ({
+            isOpenCategories:
+                typeof isOpenCategories === 'function'
+                    ? isOpenCategories(state.isOpenCategories)
+                    : isOpenCategories,
+        })),
+
     getCategories: async () => {
         try {
             const response = await CategoriesService.getCategories();
             set({ categories: response.data });
+            return response;
+        } catch (error: any) {
+            throw error;
+        }
+    },
+
+    getOneCategory: async (id: string | null) => {
+        try {
+            const response = await CategoriesService.getOneCategory(id);
             return response;
         } catch (error: any) {
             throw error;
