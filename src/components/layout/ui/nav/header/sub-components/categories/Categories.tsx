71e23ff9e3f2b84c7d9e3e68163a19a53231fb21
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-17 23:29:54 +0300

    - worked categories components - worked with categories styles

--- src/components/layout/ui/nav/header/sub-components/categories/Categories.tsx
+++ src/components/layout/ui/nav/header/sub-components/categories/Categories.tsx
@@ -1,30 +1,26 @@
 'use client';
-import React, { useState } from 'react';
+import React, { FC } from 'react';
 import styles from './Categories.module.scss';
-import { useQuery } from '@tanstack/react-query';
 import useCategoryStore from '@/api/store/CategoriesStore';
 import { ColorsEnum } from '@/utils/enums/ColorEnums';
 import { LoaderCircle } from 'lucide-react';
 
-const Categories = () => {
-    const { getCategories, setCategoryId } = useCategoryStore();
-    const [isOpenCategories, setIsOpenCategories] = useState<boolean>(false);
+interface CategoriesProps {
+    isCategoriesLoading: boolean;
+}
 
-    const { data, isLoading } = useQuery({
-        queryKey: ['categories'],
-        queryFn: () => getCategories(),
-        select: data => data.data,
-        enabled: isOpenCategories,
-    });
+const Categories: FC<CategoriesProps> = ({ isCategoriesLoading }) => {
+    const { setIsOpenCategories, isOpenCategories } = useCategoryStore();
 
     return (
         <div className={styles.categories}>
             <button
                 className={styles.categoriesBtn}
                 onClick={() => setIsOpenCategories(!isOpenCategories)}
+                disabled={isCategoriesLoading}
             >
                 <div className={styles.categoriesIconCnt}>
-                    {isLoading ? (
+                    {isCategoriesLoading ? (
                         <LoaderCircle
                             className={styles.loader}
                             size={19.67}
@@ -40,30 +36,6 @@ const Categories = () => {
                 </div>
                 Каталог товарів
             </button>
-            {isOpenCategories && (
-                <div className={styles.categoriesList}>
-                    <ul className={styles.list}>
-                        {data?.map(item => (
-                            <li key={item.id} className={styles.listItem}>
-                                <button
-                                    className={styles.listCnt}
-                                    onClick={() => setCategoryId(item.id)}
-                                >
-                                    <div className={styles.imgIcon}>
-                                        <img
-                                            src={item.iconImg}
-                                            alt={`Іконка категорії ${item.category}`}
-                                        />
-                                    </div>
-                                    <div className={styles.name}>
-                                        {item.category}
-                                    </div>
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
         </div>
     );
 };
