670c96a3febd67559f8b49da1847dfd2bc1766e4
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-17 23:30:34 +0300

    - worked with categories request

--- src/api/services/CategoriesService.ts
+++ src/api/services/CategoriesService.ts
@@ -1,11 +1,20 @@
 import { AxiosResponse } from 'axios';
 import $api from '@/api/request';
-import { CategoryResponseModel } from '@/api/models/CategoriesModels';
+import {
+    CategoryResponseModel,
+    GetOneCategoryResponseModel,
+} from '@/api/models/CategoriesModels';
 
 export default class CategoriesService {
     static async getCategories(): Promise<
         AxiosResponse<CategoryResponseModel[]>
     > {
         return $api.get<CategoryResponseModel[]>(`/category/getcategories`);
+    }
+
+    static async getOneCategory(
+        id: string | null,
+    ): Promise<AxiosResponse<GetOneCategoryResponseModel>> {
+        return $api.get(`/category/getonecategory?id=${id}`);
     }
 }
