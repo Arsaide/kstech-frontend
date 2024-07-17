726b94956e3b3cf67c900d431e24affacf0f1b70
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-17 23:30:15 +0300

    - refactoring categories models and types

--- src/api/models/CategoriesModels.ts
+++ src/api/models/CategoriesModels.ts
@@ -5,35 +5,13 @@ export interface CategoryResponseModel {
     mainImg: string;
 }
 
-export interface EditCategoryResponseModel {
-    id: string;
-    newName: string;
-    img: string | File | null;
-    token: string | undefined;
-}
-
 export interface GetOneCategoryResponseModel {
     category: CategoryResponseModel;
     subcategory: SubcategoryResponseModel[];
 }
 
-export interface CreateCategoryResponseModel {
-    id: string;
-    category: string;
-    img: string;
-    subcategory: string[];
-}
-
-export interface CreateSubcategoryResponseModel {
-    id: string;
-    category: string;
-    img: string;
-    subcategory: string;
-}
-
 export interface SubcategoryResponseModel {
     id: string;
-    category: string;
     iconImg: string;
     mainImg: string;
     subcategory: string;
