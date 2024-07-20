4669dc02e98bf6b6e537c9fafeab828ac5e5cdf1
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-21 00:45:43 +0300

    - added search request - worked with store

--- src/api/services/ProductService.ts
+++ src/api/services/ProductService.ts
@@ -3,9 +3,14 @@ import $api from '@/api/request';
 import { AllProductResponseModel } from '@/api/models/ProductsModels';
 
 export default class ProductService {
-    static async getProductsList(
-        page: number,
-    ): Promise<AxiosResponse<AllProductResponseModel>> {
+    static async getProductsList(page: number): Promise<AxiosResponse<AllProductResponseModel>> {
         return $api.get<AllProductResponseModel>(`/products/get?page=${page}`);
+    }
+
+    static async searchProducts(
+        page: number,
+        query: string,
+    ): Promise<AxiosResponse<AllProductResponseModel>> {
+        return $api.get<AllProductResponseModel>(`/products/search?page=${page}&query=${query}`);
     }
 }
--- src/api/store/ProductStore.ts
+++ src/api/store/ProductStore.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand';
 import ProductService from '@/api/services/ProductService';
 import { AllProductResponseModel } from '@/api/models/ProductsModels';
+import { AxiosResponse } from 'axios';
 
 interface ProductStoreTypes {
     products: AllProductResponseModel | null;
     getProducts: (page: number) => Promise<void>;
+    searchProducts: (query: string) => Promise<AxiosResponse<AllProductResponseModel>>;
 }
 
 const useProductsStore = create<ProductStoreTypes>(set => ({
@@ -14,6 +16,16 @@ const useProductsStore = create<ProductStoreTypes>(set => ({
         try {
             const response = await ProductService.getProductsList(page);
             set({ products: response.data });
+        } catch (error: any) {
+            throw error;
+        }
+    },
+
+    searchProducts: async (query: string) => {
+        try {
+            const page = parseInt(<string>localStorage.getItem('page')) || 1;
+            const response = await ProductService.searchProducts(page, query);
+            return response;
         } catch (error: any) {
             throw error;
         }
