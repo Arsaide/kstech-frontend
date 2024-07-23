b47fb37b9d236bace245499649322deabaefd0d7
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-23 22:58:43 +0300

    - added new sections to main page

--- src/components/pages/main-page/MainPage.tsx
+++ src/components/pages/main-page/MainPage.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import ServicesList from '@/components/pages/main-page/components/services-list/ServicesList';
 import CatalogList from '@/components/pages/main-page/components/catalog-list/CatalogList';
+import ProductsList from '@/components/pages/main-page/components/products-list/ProductsList';
 
 const MainPage = () => {
     return (
         <>
             <CatalogList />
             <ServicesList />
+            <ProductsList />
         </>
     );
 };
