b8d8f95af3519fa21c3dc01d2157083178ab7678
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-23 14:22:50 +0300

    - fixed styles and number of skeleton items

--- src/components/pages/main-page/components/catalog-list/catalog-list-skeleton/CatalogListSkeleton.tsx
+++ src/components/pages/main-page/components/catalog-list/catalog-list-skeleton/CatalogListSkeleton.tsx
@@ -4,7 +4,7 @@ import styles from './CatalogListSkeleton.module.scss';
 const CatalogListSkeleton = () => {
     return (
         <ul className={styles.produtcsList}>
-            {Array.from({ length: 10 }).map((_, index) => (
+            {Array.from({ length: 24 }).map((_, index) => (
                 <li className={styles.listItem} key={index}>
                     <div className={styles.skeleton}></div>
                 </li>
