cf125ebcdb41fb32a1a423725a062012c3098cc1
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-27 18:51:27 +0300

    - added pagination styles

--- src/components/pages/popular-products-page/pagination/Pagination.tsx
+++ src/components/pages/popular-products-page/pagination/Pagination.tsx
@@ -1,5 +1,8 @@
 'use client';
 import React, { FC } from 'react';
+import styles from './Pagination.module.scss';
+import classNames from 'classnames';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface PaginationProps {
     totalPages: number | undefined;
@@ -9,16 +12,31 @@ interface PaginationProps {
 
 const Pagination: FC<PaginationProps> = ({ totalPages, currentPage, onPageChange }) => {
     return (
-        <div>
+        <div className={styles.pagination}>
+            <button
+                className={classNames(styles.prevBtn, styles.btn)}
+                onClick={() => onPageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                <ChevronLeft size={16} />
+            </button>
             {Array.from({ length: totalPages || currentPage }, (_, i) => (
                 <button
                     key={i + 1}
                     onClick={() => onPageChange(i + 1)}
                     disabled={currentPage === i + 1}
+                    className={classNames(styles.pagBtn, styles.btn)}
                 >
                     {i + 1}
                 </button>
             ))}
+            <button
+                className={classNames(styles.nextBtn, styles.btn)}
+                onClick={() => onPageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+            >
+                <ChevronRight size={16} />
+            </button>
         </div>
     );
 };
