9bf512ec73a8e127b0ca17a38b44780689374503
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-07-31 20:12:34 +0300

    - updated price and discount functions

--- src/utils/discountPriceCalc.ts
+++ src/utils/discountPriceCalc.ts
@@ -1,4 +1,7 @@
-export const discountPriceCalc = (price: string | number, discount: string | number) => {
+export const discountPriceCalc = (
+    price: string | number | undefined,
+    discount: string | number | undefined,
+) => {
     const parsedPrice = parseFloat(price as string);
     const parsedDiscount = parseFloat(discount as string);
 
